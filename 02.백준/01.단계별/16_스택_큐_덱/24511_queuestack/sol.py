bf03ca37a31584897b25212cdcbed67c5e24f5f5
Subiniiie <subin57419@gmail.com>
2024-08-31 11:21:52 +0900

    백준_24511_queuestack 입력값 받음

---
+++ 02.백준/01.단계별/16_스택_큐_덱/24511_queuestack/sol.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline().rstrip())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline().rstrip())
+C = list(map(int, sys.stdin.readline().split()))
+
+print(A)
+print(B)
+print(C)
