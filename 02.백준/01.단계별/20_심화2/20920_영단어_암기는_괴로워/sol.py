b73924ac2c313a140df8b7bb6e24c2fdf7160d1a
Subiniiie <subin57419@gmail.com>
2024-09-12 23:47:40 +0900

    백준_20920_영단어 암기는 괴로워

--- 02.백준/01.단계별/20_심화2/20920_영단어_암기는_괴로워/sol.py
+++ 02.백준/01.단계별/20_심화2/20920_영단어_암기는_괴로워/sol.py
@@ -1,44 +1,21 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-result = []
-dict = {}
-for _ in range(N) :
-    a = sys.stdin.readline().rstrip()
+dic = {}
 
-    if len(a) < M :
+for _ in range(N) :
+    word = sys.stdin.readline().rstrip()
+
+    if len(word) < M :
         continue
     else :
-        if a in dict :
-            dict[a] += 1
+        if word in dic :
+            dic[word] += 1
         else :
-            dict[a] = 1
+            dic[word] = 1
 
-num = []
-for value in dict.values() :
-    if value not in num :
-        num.append(value)
-num.sort(reverse=True)
-for i in num :
-    temp = []
-    for key, value in dict.items() :
-        if i == value :
-            temp.append(key)
-    temp_dict = {}
-    for j in temp :
-        if len(j) in temp_dict :
-            temp_dict[len(j)].append(j)
-        else :
-            temp_dict[len(j)] = [j]
-    sorted_temp = sorted(temp_dict, reverse=True)
-    last_temp = []
-    for w in range(len(sorted_temp)) :
-        if w != len(sorted_temp) - 1 :
-            if len(sorted_temp[w]) != len(sorted_temp[w+1]) :
-                print(sorted_temp[w])
-            else :
-                last_temp.append(sorted_temp[w])
+dic = sorted(dic.items(), key = lambda x : (-x[1], -len(x[0]), x[0]))
 
-
-
+for i in dic :
+    print(i[0])
 
