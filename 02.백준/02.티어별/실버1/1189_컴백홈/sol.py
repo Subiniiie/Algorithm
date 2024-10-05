dccd27abc93c846e0370382e1cd293f7da52d845
Subiniiie <subin57419@gmail.com>
2024-10-05 11:43:16 +0900

    백준_1189_컴백홈

---
+++ 02.백준/02.티어별/실버1/1189_컴백홈/sol.py
@@ -0,0 +1,40 @@
+import sys
+
+R, C, K = map(int, sys.stdin.readline().split())
+
+arr = []
+for _ in range(R) :
+    arr.append(list(sys.stdin.readline().strip()))
+
+
+drow = [-1, 0, 1, 0]
+dcol = [0, 1, 0, -1]
+
+
+def dfs(row, col) :
+    global result
+    if row == 0 and col == C - 1 :
+        if arr[row][col] == K :
+            result += 1
+            return
+        else :
+            return
+
+    if row <= -1 or row >= R or col <= -1 or col >= C :
+        return
+    else :
+        for i in range(4) :
+            temp_row = row + drow[i]
+            temp_col = col + dcol[i]
+            if 0 <= temp_row < R and 0 <= temp_col < C :
+                if arr[temp_row][temp_col] == '.' :
+                    arr[temp_row][temp_col] = arr[row][col] + 1
+                    dfs(temp_row, temp_col)
+                    arr[temp_row][temp_col] = '.'
+
+
+result = 0
+arr[R - 1][0] = 1
+
+dfs(R - 1, 0)
+print(result)
