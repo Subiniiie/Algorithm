8d284c1292964c3e57a4023f8be6b261e46ad9b2
Subiniiie <subin57419@gmail.com>
2025-04-18 08:36:06 +0900

    10430_나머지

---
+++ 03.2025/01.백준/01.입출력과_사칙연산/10430_나머지/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(process.platform === 'linux' ?
+    '/dev/stdin' : path.join(__dirname, 'input.txt')
+).toString().trim().split(" ").map(Number);
+
+const A = input[0];
+const B = input[1];
+const C = input[2];
+
+console.log((A+B)%C);
+console.log(((A%C) + (B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C) * (B%C))%C);
