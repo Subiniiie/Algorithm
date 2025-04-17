866866c3701e0d447c99cde237fee3c34179537f
Subiniiie <subin57419@gmail.com>
2025-04-18 07:42:23 +0900

    백준_1000

---
+++ 03.2025/01.백준/01.입출력과_사칙연산/1000_A+B/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(process.platform === "linux" ? 
+    "/dev/stdin" : path.join(__dirname, 'input.txt')).toString().trim().split(" ").map(Number);
+
+const A = input[0];
+const B = input[1];
+
+console.log(A+B);
