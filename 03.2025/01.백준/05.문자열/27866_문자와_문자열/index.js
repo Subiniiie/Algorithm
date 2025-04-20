161120f8fa3160741ed10c77eba46138b694edc0
Subiniiie <subin57419@gmail.com>
2025-04-20 11:51:55 +0900

    백준_27866

---
+++ 03.2025/01.백준/05.문자열/27866_문자와_문자열/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(process.platform === 'linux' ?
+    '/dev/stdin' : path.join(__dirname, 'input.txt')
+).toString().trim().split("\n");
+
+const t = Number(input[1]);
+console.log(input[0][t - 1])
