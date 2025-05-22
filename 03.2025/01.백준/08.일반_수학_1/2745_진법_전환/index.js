02f5c0b4e7c2363aaceb7967378388f4ebf74e54
Subiniiie <subin57419@gmail.com>
2025-05-22 11:14:31 +0900

    백준_2745

--- 03.2025/01.백준/08.일반_수학_1/2745_진법_전환/index.js
+++ 03.2025/01.백준/08.일반_수학_1/2745_진법_전환/index.js
@@ -5,34 +5,24 @@ const input = fs.readFileSync(process.platform === 'linux' ?
     '/dev/stdin' : path.join(__dirname, 'input.txt')
 ).toString().trim().split(" ")
 
-const N = input[0];
-const B = Number(input[1]);
-const l = N.length;
-
-function binaryNumber(N, l) {
-    let a = 0;
-    for (let i = l - 1; i >= 0; i--) {
-        const t = l - 1 - i;
-        if (N[i] === '1') {
-            a += 2 ** t;
-        };
-    };
-    return a;
-};
-
-function octalNumber(N, l) {
-    let a = 0;
-    for (let i = l - 1; i >= 0; i--) {
-        const t = l - 1 - i;
-        a += Number(N[i]) * (8 ** t);
-    };
-    return a;
+const alpha = {
+    0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
+    A: 10, B: 11, C : 12, D: 13, E: 14, F: 15, G: 16, H: 17, I: 18,
+    J: 19, K: 20, L: 21, M: 22, N: 23, O: 24, P: 25, Q: 26, R: 27, S: 28,
+    T: 29, U: 30, V: 31, W: 32, X: 33, Y: 34, Z: 35 
 };
 
 
-
-if (B === 2) {
-    console.log(binaryNumber(N, l));
-} else if (B === 8) {
-    console.log(octalNumber(N, l));
+function changeTen (B, N) {
+    const l = N.length;
+    let a = 0;
+    for (let i = l - 1; i >= 0; i--) {
+        const m = l - 1 - i;
+        a += alpha[N[i]] * (B ** m)
+    }
+    return a;
 }
+
+const B = input[1];
+const N = input[0];
+console.log(changeTen(B, N));
