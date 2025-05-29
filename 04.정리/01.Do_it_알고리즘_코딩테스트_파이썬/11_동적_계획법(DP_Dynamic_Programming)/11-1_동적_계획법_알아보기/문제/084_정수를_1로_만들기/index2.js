94d656db72a275e255e2e10a18091347950f28b6
Subiniiie <subin57419@gmail.com>
2025-05-29 10:28:08 +0900

    동적 계획법 084, 085 복습

---
+++ 04.정리/01.Do_it_알고리즘_코딩테스트_파이썬/11_동적_계획법(DP_Dynamic_Programming)/11-1_동적_계획법_알아보기/문제/084_정수를_1로_만들기/index2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(process.platform === 'linux' ?
+    '/dev/stdin' : path.join(__dirname, 'input.txt')
+).toString().trim();
+
+const N = Number(input);
+
+let D = new Array(N+1).fill(0);
+
+for (let i = 2; i <= N; i++) {
+    D[i] = D[i-1] + 1;
+    if (i % 2 === 0) {
+        D[i] = Math.min(D[i], D[i/2] + 1)
+    }
+    if (i % 3 === 0) {
+        D[i] = Math.min(D[i], D[i/3] + 1)
+    }
+};
+
+console.log(D[N])
---
+++ 04.정리/01.Do_it_알고리즘_코딩테스트_파이썬/11_동적_계획법(DP_Dynamic_Programming)/11-1_동적_계획법_알아보기/문제/085_퇴사_준비하기/index3.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(process.platform === 'linux' ?
+    '/dev/stdin' : path.join(__dirname, 'input.txt')
+).toString().trim().split("\n").map((el) => el.trim().split(' ').map(Number))
+
+const N = input[0][0];
+let T = new Array(N+1).fill(0);
+let P = new Array(N+1).fill(0);
+
+let D = new Array(N+2).fill(0);
+
+for (let i = 1; i < N+1; i++) {
+    const [t, p] = input[i];
+    T[i] = t;
+    P[i] = p;
+};
+
+for (let i = N; i > 1; i--) {
+    if (i + T[i] > N + 1) {
+        D[i] = D[i+1];
+    } else {
+        D[i] = Math.max(D[i], D[i+T[i]] + P[i]);
+    }
+};
+
+console.log(D[1])
