02393c2362fe20446ec26568abd728c6a4dbb510
Subiniiie <subin57419@gmail.com>
2025-06-24 23:57:59 +0900

    백준_12845 푸는 중

---
+++ 03.2025/01.백준/100.티어별/실버3/12845_모두의_마블/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs.readFileSync(process.platform === 'linux' ?
+    "/dev/stdin" : path.join(__dirname, "input.txt")
+).toString().trim().split("\n").map((el) => el.trim().split(" ").map(Number));
+
+const n = input[0][0];
+const cards = input[1];
+
+let gold = 0;
+
+let maxNum = 0;
+let maxIdx = 0;
+
+let temp = 0;
+
+for (let i = 0; i < n; i++) {
+    if (maxNum < cards[i]) {
+        maxNum = cards[i];
+        maxIdx = i;
+    };
+};
+
+while (cards.length > 0) {
+    if (cards.length > 1) {
+        if (maxIdx > 0 && maxIdx < n - 1) {
+            if (cards[maxIdx-1] > cards[maxIdx+1]) {
+                gold += cards[maxIdx-1];
+                cards.splice(maxIdx-1, 1);
+            } else {
+                gold += cards[maxIdx+1];
+                cards.splice(maxIdx+1, 1);
+            };
+        } else if (maxIdx === 0) {
+            gold += cards[maxIdx+1];
+            cards.splice(maxIdx+1, 1);
+        } else {
+            gold += cards[maxIdx-1];
+            cards.splice(maxIdx-1, 1);
+        }
+    } else {
+        gold += cards[maxIdx];
+        cards.splice(maxIdx, 1);
+    };
+};
+
+console.log(gold)
